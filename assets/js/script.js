74ed296073c179b9c46c2108a0718077640571d6
SimonMertins <147529923+SimonMertins@users.noreply.github.com>
2023-11-10 14:34:11 +0100

    display questions addtion game

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,20 +11,31 @@ document.addEventListener("DOMContentLoaded", function(){
         } else {
             let gameType = this.getAttribute("data-type");
             alert('You clicked ${gameType}');
+            runGame(gameType);
         }
     })
-    }
+   }
+
+    runGame("addition");
+
 })
 
 /**
  * The main gamee "loop" called first when the script is loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     //creates 2 random numbers betweeen 1 and 25
     let num1 = math.floor(math.random() * 25) + 1;
     let num2 = math.floor(math.random() * 25) + 1;
+
+    if (gameType === "addition") {
+        displayAdditonQuestion(num1, num2);
+    } else {
+        alert('Unknown game type: ${gametype}');
+        throw 'unknown game type  ${gameType}. Aborting!';
+    }
 
 }
 
@@ -44,8 +55,10 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditonQuestion() {
-
+function displayAdditonQuestion(operand1, operand2) {
+    document.getElementById('oprand1').textContent = operand1;
+    document.getElementById('oprand2').textContent = operand2;
+    document.getElementById('operator').textContent = "+";
 }
 
 function displaySubtractQuestion() {
